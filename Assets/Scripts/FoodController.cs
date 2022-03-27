266f973a69dee7bdc958f99da5a333bf5e1e5598
arindambharati <arindambharati@gmail.com>
2022-03-27 20:50:45 +0530

    FoodController randomization implemented

---
+++ Assets/Scripts/FoodController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FoodController : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D gridArea;
+
+    private void Start()
+    {
+        RandomizeFoodPosition();
+    }
+    private void RandomizeFoodPosition()
+    {
+        Bounds bounds = this.gridArea.bounds;
+
+        float x_Position = Random.Range(bounds.min.x, bounds.max.x);
+        float y_Position = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector2(Mathf.Round(x_Position),Mathf.Round(y_Position));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            RandomizeFoodPosition();
+        }
+    }
+}
