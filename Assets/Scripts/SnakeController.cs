5203f45899ae12020e9d2ebe40bfe78b3d78434c
arindambharati <arindambharati@gmail.com>
2022-03-27 19:01:10 +0530

    Snake movement implemented

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// This script is implementing the Snake Movement.
+/// Taking input in the update() with Vector2 to represent the positions of the snake
+/// this.transform.position.x || this.transform.position.y is actually taking the current position of the snake gameobject
+/// Round() is used as the snake game is grid based hence, rounding off the numbers of the position actually helps in doing that.
+/// </summary>
 public class SnakeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _direction;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) { 
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            _direction = Vector2.right;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        SnakeMovement();
+    }
+
+    private void SnakeMovement()
+    {
+        this.transform.position = new Vector2(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y
+            );
     }
 }
